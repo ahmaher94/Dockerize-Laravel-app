455b4b325e3bdd18adbe77bbf04f4056474b95bc
ah-maher94 <ahmed.maher0094@gmail.com>
2024-08-10 14:00:15 +0300

    setting up redis service use redis to cache posts

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index(){
-        return Post::all();
+        if(Cache::store('redis')->get('posts')){
+            echo "from cache";
+            $posts = Cache::store('redis')->get('posts');
+        }else{
+            echo "not from cache";
+            $posts = Post::all();
+            Cache::store('redis')->put('posts', $posts, 60);
+        }
+        return $posts;
     }
 
     public function show(Post $post){
